087652fdb73d24693912f802560dcc0239a12183
Jose Zaccaro <a.zaccaro42@gmail.com>
2024-06-28 13:52:09 -0300

    FT: rutas de hoteles y ciudades. TODO: rutas de rooms.

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import jsonify, request
 from app.models.city import City
+from app.models.hotel import Hotel
 
 def index():
     return '<h1>Hola mundo con flask 🐍</h1>'
@@ -44,3 +45,51 @@ def delete_city(city_id):
         return jsonify({'message': 'City not found'}), 404
     city.delete()
     return jsonify({'message': 'City deleted successfully'})
+
+def get_all_hotels():
+    try:
+        hotels = Hotel.get_all()
+        list_hotels = [hotel.serialize() for hotel in hotels]
+        return jsonify(list_hotels)
+    except Exception as identifier:
+        print(identifier)
+        return jsonify([])
+
+def create_hotel():
+    #recepcionando los datos enviados en la peticion en formato JSON
+    data = request.json
+    new_hotel = Hotel(
+        name = data['name'],
+        address = data['address'],
+        description = data['description'],
+        image = data['image'],
+        id_city = data['id_city'],
+    )
+    new_hotel.save()
+    return jsonify({'message':'Hotel created'}), 201
+    
+def update_hotel(hotel_id):
+    hotel = Hotel.get_by_id(hotel_id)
+    if not hotel:
+        return jsonify({'message': 'Hotel not found'}), 404
+    data = request.json
+    hotel.name = data['name']
+    hotel.address = data['address']
+    hotel.description = data['description']
+    hotel.image = data['image']
+    hotel.id_city = data['id_city']
+    hotel.save()
+    return jsonify({'message': 'Hotel updated successfully'})
+
+def get_hotel(hotel_id):
+    hotel = Hotel.get_by_id(hotel_id)
+    if not hotel:
+        return jsonify({'message': 'Hotel not found'}), 404
+    return jsonify(hotel.serialize())
+
+def delete_hotel(hotel_id):
+    hotel = Hotel.get_by_id(hotel_id)
+    if not hotel:
+        return jsonify({'message': 'Hotel not found'}), 404
+    hotel.delete()
+    return jsonify({'message': 'Hotel deleted successfully'})
--- run.py
+++ run.py
@@ -16,5 +16,11 @@ app.route('/api/cities/', methods=['POST'])(create_city)
 app.route('/api/cities/<int:city_id>', methods=['PUT'])(update_city)
 app.route('/api/cities/<int:city_id>', methods=['DELETE'])(delete_city)
 
+app.route('/api/hotels/', methods=['GET'])(get_all_hotels)
+app.route('/api/hotels/<int:hotel_id>', methods=['GET'])(get_hotel)
+app.route('/api/hotels/', methods=['POST'])(create_hotel)
+app.route('/api/hotels/<int:hotel_id>', methods=['PUT'])(update_hotel)
+app.route('/api/hotels/<int:hotel_id>', methods=['DELETE'])(delete_hotel)
+
 if __name__=='__main__':
     app.run(debug=True)
